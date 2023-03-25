75cae0c408d784b574ede8d21579f57357de0550
David Santos <89614359+DaviidSantos@users.noreply.github.com>
2023-03-24 22:13:48 -0300

    Configurando o ambiente do Backend

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { corsOptions } from './security/corsOptions';
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan('combined'));
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on  http://localhost:${PORT}`);
+});
