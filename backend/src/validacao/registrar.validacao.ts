c85a44c70a998784af9a2130e9a8d9328829c648
David Santos <89614359+DaviidSantos@users.noreply.github.com>
2023-03-25 12:49:16 -0300

    Configuração das mensagens de validação

--- backend/src/validacao/registrar.validacao.ts
+++ backend/src/validacao/registrar.validacao.ts
@@ -1,9 +1,9 @@
 import * as yup from 'yup';
 
 export const registrarValidacao = yup.object({
-    nome: yup.string().required(),
-    email: yup.string().email().required(),
-    senha: yup.string().required().min(8),
-    confirmarSenha: yup.string().required().oneOf([yup.ref('senha')], 'Senhas não conferem'),
-    cargos: yup.array().of(yup.string().required()).required(),
+    nome: yup.string().required("Nome é um campo obrigatório"),
+    email: yup.string().email("Email inválido").required("Email é um campo obrigatório"),
+    senha: yup.string().required("Senha é um campo obrigatório").min(8, "Senha deve ter no mínimo 8 caracteres"),
+    confirmarSenha: yup.string().required("Confirmar Senha é um campo obrigatório").oneOf([yup.ref('senha')], 'Senhas não conferem'),
+    cargos: yup.array().of(yup.string().required()).required("Cargos é um campo obrigatório"),
 })
